7dd2f34301ea2121326c0612635442cfbede2bdf
Silas Waxter <silaswaxter@gmail.com>
2021-08-14 11:00:22 -0700

    Updated desktop display xrandr script

--- Display/desktop.sh
+++ Display/desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 1920x1080 --pos 3440x603 --rotate normal --output HDMI-A-0 --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off
+xrandr --output eDP --primary --mode 1280x720 --pos 3440x973 --rotate normal --output HDMI-A-0 --mode 3440x1440 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off
