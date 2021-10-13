bc69bb6693ca1e7bfd8af552f0f05911271d3933
Silas Waxter <silaswaxter@gmail.com>
2021-10-13 09:48:12 -0700

    Changed Wallpaper

--- Wallpaper/update.sh
+++ Wallpaper/update.sh
@@ -3,7 +3,7 @@
 #get directory of this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-wallpaper_image=$DIR/SolidFill.png
+wallpaper_image=/usr/share/slick-greeter/slick-greeter.png
 
 # Set Wallpaper
 nitrogen --set-scaled $wallpaper_image
